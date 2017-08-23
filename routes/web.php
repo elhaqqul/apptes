b3e22f593ff641675c05cab71d9dbc79d79b802c
elhaqqul <meggiekurniap@gmail.com>
2017-08-23 17:35:13 +0700

    relationship

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,36 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use App\UserModel;
+use App\Status;
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/tes', function () {
-    return view('welcome');
+    $user_array = array('name'=>'elhaqqul',
+    					'email'=>'meggieelhaqqul@ymail.com',
+    					'password'=>md5('123'),
+    					'address'=>'Jalan Tanjung Duren Selatan'	
+    			  );
+    $user = new UserModel;
+    $user->insert($user_array);
+});
+
+Route::get('/getByid/{id}','Home@getData')->name('getData');
+
+Route::get('/InsertoneToMany', function(){
+	// $user = new UserModel;
+	// $user->name="Tes";
+	// $user->email="Tes@gmail.com";
+	// $user->address="Tes Address";
+	// $user->password=md5("321");
+	// $user->save();
+	// $id = $user->id;
+	
+	$user = UserModel::find(2);
+	$status = new Status(['status_name'=>'tes status']);
+	//$status->status="tes Status";
+	$user->status_user()->save($status);
+	
 });
